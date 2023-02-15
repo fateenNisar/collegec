4c4ae30796486fe20b7ddf053c5ae2e089002e5c
fateenNisar <fateennisar121@gmail.com>
2023-02-15 23:48:31 +0530

    inital

---
+++ insert.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a[N] , i ;
+    for (i = 0 ; i <  N ; i++ )
+    {
+        printf("enter elements");
+        scanf("%d" ,&a[i]);  
+    }
+    printf("enter element to be inserted at end");
+    scanf("%d" ,&a[i]);  
+    return 0;
+}
